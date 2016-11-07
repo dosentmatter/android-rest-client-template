22362c01fd64aa806329c381c6b4cdaaab7e28a1
Kevin Lau <kelau1993@gmail.com>
2016-11-06 21:05:45 -0800

    Refactor duplicate code in TweetsListFragment

--- app/src/main/java/com/codepath/apps/simpletwitter/fragments/TweetsListFragment.java
+++ app/src/main/java/com/codepath/apps/simpletwitter/fragments/TweetsListFragment.java
@@ -4,32 +4,56 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.codepath.apps.simpletwitter.R;
+import com.codepath.apps.simpletwitter.TwitterApplication;
 import com.codepath.apps.simpletwitter.adapters.TweetArrayAdapter;
 import com.codepath.apps.simpletwitter.models.Tweet;
+import com.codepath.apps.simpletwitter.network.NetworkTools;
+import com.codepath.apps.simpletwitter.network.TwitterClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class TweetsListFragment extends Fragment {
+import cz.msebera.android.httpclient.Header;
 
-    private ListView lvTweets;
-    private ArrayList<Tweet> tweetsList;
-    private TweetArrayAdapter tweetsAdapter;
+public abstract class TweetsListFragment extends Fragment {
 
-    private SwipeRefreshLayout swipeContainer;
+    protected static final String TAG = TweetsListFragment.class
+                                                          .getSimpleName();
+
+    protected static final int TWEET_COUNT = 25;
+
+    protected TwitterClient client;
+
+    protected ListView lvTweets;
+    protected ArrayList<Tweet> tweetsList;
+    protected TweetArrayAdapter tweetsAdapter;
+
+    protected SwipeRefreshLayout swipeContainer;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        client = TwitterApplication.getRestClient();
+
         tweetsList = new ArrayList<>();
         tweetsAdapter = new TweetArrayAdapter(getActivity(), tweetsList);
+
+        populateTimeline();
     }
 
     @Nullable
@@ -58,6 +82,53 @@ public class TweetsListFragment extends Fragment {
 
 //        setupListViewListeners();
     }
+
+    protected void populateTimeline() {
+        Map<String, String> params = new HashMap<>();
+        params.put("count", String.valueOf(TWEET_COUNT));
+        populateTimeline(params);
+    }
+
+    protected void populateTimeline(Map<String, String> params) {
+        if (NetworkTools.isOnline()) {
+            AsyncHttpResponseHandler timelineHandler
+                = new JsonHttpResponseHandler() {
+                      @Override
+                      public void onSuccess(int statusCode, Header[] headers,
+                                            JSONArray response) {
+                          List<Tweet> tweetResults
+                              = Tweet.fromJSONArray(response);
+                          Tweet.saveTweets(tweetResults);
+                          addAll(tweetResults);
+                      }
+
+                      @Override
+                      public void onFailure(int statusCode, Header[] headers,
+                                            String responseString,
+                                            Throwable throwable) {
+                          Log.d(TAG, responseString);
+                      }
+                  };
+
+            RequestParams requestParams
+                = NetworkTools.convertToRequestParams(params);
+            getTimeLine(requestParams, timelineHandler);
+        } else {
+//            QueryTransaction.QueryResultListCallback<Tweet> timelineHandler
+//                = new QueryTransaction.QueryResultListCallback<Tweet>() {
+//                      @Override
+//                      public void
+//                      onListQueryResult(QueryTransaction transaction,
+//                                        @Nullable List<Tweet> tResult) {
+//                          addAll(tResult);
+//                      }
+//                  };
+//            Tweet.getHomeTimeLine(params, timelineHandler);
+        }
+    }
+
+    abstract protected void getTimeLine(RequestParams params,
+                                        AsyncHttpResponseHandler handler);
 //
 //    private void setupSwipeContainerListeners() {
 //        SwipeRefreshLayout.OnRefreshListener swipeContainerRefreshListener
